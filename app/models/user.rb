2a95780900c010933071ddf6a8119eaf9700a6d5
Abdu <abdulrahmanammar17@gmail.com>
2025-11-14 15:43:08 +0300

    creates users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
 
     has_secure_password
+
+    has_one :membership
+
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
 end
