17ddd18b6ea87e93a4eb4089a8c5a96ad84c2aef
Abdu <abdulrahmanammar17@gmail.com>
2025-11-25 06:53:40 +0300

    restrict CORS origins to localhost for development environment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,10 +4,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # Allow requests from any origin.
     # For production, change '*' to your React app's domain.
-    origins '*' 
+    origins "http://localhost:5173"
+     
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
