aee710285f0c27393dec8a7f426fe75bc0fc1f57
Abdu <abdulrahmanammar17@gmail.com>
2025-12-02 14:54:00 +0300

    feat: update schema to add new tables for attendances, bookings, class bookings, and payments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_28_050538) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_01_114354) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -39,6 +39,36 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_28_050538) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "attendances", force: :cascade do |t|
+    t.datetime "checked_in_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "class_booking_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["class_booking_id"], name: "index_bookings_on_class_booking_id"
+    t.index ["user_id", "class_booking_id"], name: "index_bookings_on_user_id_and_class_booking_id", unique: true
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "class_bookings", force: :cascade do |t|
+    t.integer "capacity", default: 20, null: false
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.string "duration"
+    t.string "image_url"
+    t.string "instructor"
+    t.string "name"
+    t.string "time"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "memberships", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.integer "plan_id", null: false
@@ -46,6 +76,19 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_28_050538) do
     t.integer "user_id", null: false
     t.index ["plan_id"], name: "index_memberships_on_plan_id"
     t.index ["user_id"], name: "index_memberships_on_user_id"
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.integer "amount_cents"
+    t.datetime "created_at", null: false
+    t.string "currency"
+    t.string "description"
+    t.string "payment_method"
+    t.string "status"
+    t.string "transaction_id"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_payments_on_user_id"
   end
 
   create_table "plans", force: :cascade do |t|
@@ -74,12 +117,18 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_28_050538) do
     t.string "email"
     t.string "name"
     t.string "password_digest"
+    t.string "qr_token"
     t.string "role"
     t.datetime "updated_at", null: false
+    t.index ["qr_token"], name: "index_users_on_qr_token"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "attendances", "users"
+  add_foreign_key "bookings", "class_bookings"
+  add_foreign_key "bookings", "users"
   add_foreign_key "memberships", "plans"
   add_foreign_key "memberships", "users"
+  add_foreign_key "payments", "users"
 end
