53ffe0886f11d85a0d10c44aa00d7573863660ad
Abdu <abdulrahmanammar17@gmail.com>
2025-12-02 14:53:53 +0300

    feat: update PayPal SDK initialization to use new client configuration

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,13 +1,10 @@
 # config/initializers/paypal.rb
 require 'paypal_server_sdk'
 
-PayPal::Server::SDK.configure do |config|
-  config.client_id = Rails.application.credentials.paypal[:client_id]
-  config.client_secret = Rails.application.credentials.paypal[:client_secret]
-  
-  # Change to "PRODUCTION" when you go live
-  config.environment = "SANDBOX" 
-end
-
-# Create a global client helper we can use in controllers
-PAYPAL_CLIENT = PayPal::Server::SDK::PayPalClient.new
+PAYPAL_CLIENT = PaypalServerSdk::Client.new(
+  client_credentials_auth_credentials: PaypalServerSdk::ClientCredentialsAuthCredentials.new(
+    o_auth_client_id: Rails.application.credentials.dig(:paypal, :client_id),
+    o_auth_client_secret: Rails.application.credentials.dig(:paypal, :client_secret)
+  ),
+  environment: PaypalServerSdk::Environment::SANDBOX
+)
